fb8bc659e9f482359a6f2a807872de8a394452c6
Ronil999 <ronillakhani999@gmail.com>
2025-03-06 11:23:02 +0530

    full screen mode update

--- app/order-pos/page.jsx
+++ app/order-pos/page.jsx
@@ -3,12 +3,30 @@
 import { useState, useEffect } from "react";
 import Navbar from "../dashboard/navbar/page";
 
-export default function order_pos() {
+export default function OrderPOS() {
   const [active, setActive] = useState("Kitchen");
+
+  useEffect(() => {
+    // Function to enter fullscreen mode
+    const enterFullScreen = () => {
+      if (document.documentElement.requestFullscreen) {
+        document.documentElement.requestFullscreen();
+      } else if (document.documentElement.mozRequestFullScreen) {
+        document.documentElement.mozRequestFullScreen(); // Firefox
+      } else if (document.documentElement.webkitRequestFullscreen) {
+        document.documentElement.webkitRequestFullscreen(); // Chrome, Safari, Opera
+      } else if (document.documentElement.msRequestFullscreen) {
+        document.documentElement.msRequestFullscreen(); // IE/Edge
+      }
+    };
+
+    // Enter fullscreen when the component mounts
+    enterFullScreen();
+  }, []);
+
   return (
     <>
       <div className="md:w-64">
-        <Navbar active={active} setActive={setActive} />
       </div>
       <div>order-pos</div>
     </>
